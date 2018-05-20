8096406a88dec98a0033b6e1afb8cf655984714f
pallavidughrekar <38434226+pallavidughrekar@users.noreply.github.com>
2018-05-20 16:33:08 +0100

    Admin front end

---
+++ views/Admin/index.php
@@ -0,0 +1,42 @@
+<!DOCKTYPE html>
+<head>
+    <link href="adminstyle.css" rel="stylesheet" type="text/css"/>
+    <title> Admin Dashboard </title>   
+</head>
+
+<body>
+    <div id="header">
+        <center><img src="../pictures/admin.png" alt="adminlogo" id="adminlogo"/><br> <h2>Welcome Admin!</h2></center>
+    </div>
+    
+    
+    <div id="sidebar">
+        
+        <ul style="list-style-type:none">
+                <li>Manage articles</li>
+                <li>Create article</li>
+                <li>Manage comments</li>
+                <li>Manage users</li>
+                <li>Home</li>
+                <li>Log out</li>
+        </ul>
+        
+    </div>
+    
+    
+    
+    <div id="data">
+        
+        
+        
+    </div>
+    
+    <div class="footer">
+            
+                <div class="copywrite">
+                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="heart">&hearts;</i> by The Tech-sy Ladies
+             
+                </div>
+        </div>
+</body>
+</html>

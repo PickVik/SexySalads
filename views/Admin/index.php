b2e15b90b5fbc13a344a501405584889b8e49a74
pallavidughrekar <38434226+pallavidughrekar@users.noreply.github.com>
2018-05-21 23:23:50 +0100

    Admin page with all articles

--- views/Admin/index.php
+++ views/Admin/index.php
@@ -1,25 +1,40 @@
-<!DOCKTYPE html>
-<head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link href="../../public/css/admin_dashboard.css" rel="stylesheet" type="text/css"/>
-    <title> Admin Dashboard </title>   
-</head>
-<link href="../../public/css/admin_dashboard.css" rel="stylesheet" type="text/css"/>
-<body>
-    <div id="header">
-        <center><img src="admin.png" alt="adminlogo" id="adminlogo"/><br> <h2>Welcome Admin!</h2></center>
+
+
+<title> Admin Dashboard </title>  
+
+
+<div id="header" style="width: 100%;
+    height: 120px;
+    background: #8f50e7;
+    color: white;
+    padding-bottom: 5px;
+    margin-left: 10px;
+    margin-right:10px;">
+    
+    <center><img src="views/pictures/admin.png" alt="adminlogo" id="adminlogo" style="padding-top: 5px;
+    pdding-botton: 2px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50px"/><br> <h2>Welcome Admin!</h2></center>
     </div>
    
     
-    <div id="sidebar">
+<div id="sidebar" 
+    style="margin-top: 7px;
+    margin-left: 10px;
+    width: 220px;
+    height: 700px;
+    background: #8f50e7;
+    float: left;">
         
-        <ul style="list-style-type:none">
-                <li>Manage articles</li>
-                <li>Create article</li>
-                <li>Manage comments</li>
-                <li>Manage users</li>
-                <li>Home</li>
-                <li>Log out</li>
+        <ul style="list-style-type:none; padding: 40px;">
+            
+                <li><a href="">Manage articles</a></li>
+                <li><a href="">Create article</a></li>
+                <li><a href="">Manage comments</a></li>
+                <li><a href="">Manage users</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Log out</a></li>
         </ul>
         
     </div>
@@ -27,92 +42,42 @@
     
     
     <div id="data">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 col-lg-4">
-                    <div class="card">
-                        <img src="../pictures/beetrootsalad.jpg" alt="beetroot" class="img-fluid"/>
-                          <div class="card-block">
-                            <div class="card-title">
-                                <h4> Beetroot salad</h4>
-                                <div class="card-text">The original recipe called
-                                    for two beets instead of one beet and one carrot 
-                                    and chopped mint instead of cilantro. 
+       
+<div class="row">
+  <div class="col-md-9">
+      <div class='card'>
+          
+          <h2 class="content-title">All Recipes</h2>
+          
+<?php   $model = new Model();
+        $posts = $model->getPublishedPosts();
+        //print_r($posts);
+        foreach ($posts as $post):?>
+          
+<div class="post" style="margin-left: 0px;width:49.5%;display:inline-block;padding:5px">
+			<img src="<?php echo $post['image']; ?>" class="post_image" alt="" style='width:100%'>
+			<a href="single_post?post-slug=<?php echo $post['slug']; ?>">
+				<div class="post_info">
+					<h3><?php echo $post['title']; ?></h3>
+					<div class="info">
+        <span><?php echo date("F j, Y ", strtotime($post["date_created"])) ?></span>
+						<span class="read_more">Read more...</span>
+					</div>
+                                        
                                 </div>
-                                <form action="" method="post">
-
-                                <input class="btn btn-primary" type="submit" name="update" value="Update"/>
-                                <input class="btn btn-primary" type="submit" name="delete" value="Delete"/>
-
-                               </form>
-                            </div>
-                        </div>
+			</a>
+		</div>
+        <?php  endforeach; ?>
+             </div>
                     </div>
-                </div>
-           
-        
-        
-        
-        
-           
-                <div class="col-md-4 col-lg-4">
-                    <div class="card">
-                        <img src="../pictures/green-salad.jpg" alt="green salad" class="img-fluid"/>
-                        <div class="card-block">
-                            <div class="card-title">
-                                <h4> Green salad</h4>
-                                <div class="card-text">The original recipe called
-                                    for two beets instead of one beet and one carrot 
-                                    and chopped mint instead of cilantro. 
-                                </div>
-                                <form action="" method="post">
-
-                                <input class="btn btn-primary" type="submit" name="update" value="Update"/>
-                                <input class="btn btn-primary" type="submit" name="delete" value="Delete"/>
-
-                               </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            
-        
-        
-        
-            
-                <div class="col-md-4 col-lg-4">
-                    <div class="card">
-                        <img src="../pictures/vegetarian.jpg" alt="beetroot" class="img-fluid"/>
-                        <div class="card-block">
-                            <div class="card-title">
-                                <h4> Vegetarian salad</h4>
-                                <div class="card-text">The original recipe called
-                                    for two beets instead of one beet and one carrot 
-                                    and chopped mint instead of cilantro. 
-                                </div>
-                                <form action="" method="post">
-
-                                <input class="btn btn-primary" type="submit" name="update" value="Update"/>
-                                <input class="btn btn-primary" type="submit" name="delete" value="Delete"/>
-
-                               </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-         
     </div>
-        </div>
+        
+
+
     
-    <div class="footer">
-            
-                <div class="copywrite">
-                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="heart">&hearts;</i> by The Tech-sy Ladies
-             
-                </div>
-        </div>
-</body>
-</html>
- 
+   
+
+                    
+        
+    
+    

dfd4d02c8fee827a8429952cd69eb573ff982fbf
ViktM <viktoria.mezei0708@gmail.com>
2018-05-27 23:06:31 +0100

    view update

--- views/post/admin.php
+++ views/post/admin.php
@@ -1,18 +1,19 @@
     <button style="background-color: #8f50e7"><a href=../admin>Return to Admin page</a></button>
 
 
-<h1>Manage articles</h1>
+<h1 style="text-align: center; color: purple;">Manage articles</h1>
 
-<p>Here is a list of all posts</p>
-
+<h2 style="text-align: center; color: purple;">Here is a list of all posts</h2>
+<div class="container" style="color: purple;margin-top:5px;width:75%;border-style:dotted;border-width:2px;padding-bottom:10px;" >
 <?php foreach($objects as $article) { ?>
-  <p>
+  
    
    
     
-    <a href='../single_post?post-slug=<?php echo $article->slug; ?>'><?php echo $article->title; ?></a> &nbsp; &nbsp;
+    <a href='../single_post?post-slug=<?php echo $article->slug; ?>'><h3 style="color: purple;"><?php echo $article->title; ?></h3></a> &nbsp; &nbsp;
     
-    <a href='../post/delete?article_id=<?php echo $article->article_id; ?>'>Delete Post</a> &nbsp; &nbsp;
-    <a href='../post/open_edit?article_id=<?php echo $article->article_id; ?>'>Edit Post</a> &nbsp;
-  </p>
+    <a href='../post/delete?article_id=<?php echo $article->article_id; ?>'><h5 style="color: purple;">Delete Post</h5></a> 
+    <a href='../post/open_edit?article_id=<?php echo $article->article_id; ?>'><h5 style="color: purple;">Edit Post</h5></a> &nbsp;
+  
 <?php } ?> 
+  </div>

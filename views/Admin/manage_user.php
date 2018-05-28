7d60880454b210aa2df9805229b2a89d170fcee2
johasago <jo@gowithjo.online>
2018-05-28 18:48:07 +0100

    required fields added to form

--- views/Admin/manage_user.php
+++ views/Admin/manage_user.php
@@ -1,21 +1,4 @@
-<?php  $msg = "";
-if(isset($_POST['submit'])){
-foreach($_POST as $key=>$value) {
-            if(empty($_POST[$key])) {
-            $msg = "All Fields are required";
-            break;
-        }
-    
-        if ($password !== $cpassword){
-            $msg = "Password does not match";
-        
-        
-    }   else {
-        $this->model->register_new_admin($_POST['user_id'], $_POST['email'], $_POST['first_name'], $_POST['last_name'], $_POST['password'], $_POST['admin']);
-    }
-}
-}
-?>
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -24,7 +7,7 @@ foreach($_POST as $key=>$value) {
     <body>
         
         
-        <div class="container" style ="">
+        <div class="container" style ="color:#8f50e7">
    
           
        
@@ -32,11 +15,11 @@ foreach($_POST as $key=>$value) {
         
         <form action='../admin/register_new_admin' method="post" > 
         
-        Email Address:         <input class="form-control" type="email"  name="email" placeholder ="Email" /> <br>
-        Password:              <input class="form-control" type="password" name="password" placeholder ="Password"/> <br>
-        Confirm Password:      <input class="form-control" type="password"  name="cpassword" placeholder ="Confirm Password"/> <br>
-        First Name:            <input class="form-control" type="text" name="first_name" placeholder ="First Name" /> <br>
-        Last Name:           <input class="form-control" type="text" name="last_name" placeholder ="Second Name"/> <br>
+        Email Address:         <input class="form-control" type="email"  name="email" required placeholder ="Email" /> <br>
+        Password:              <input class="form-control" type="password" name="password" required placeholder ="Password"/> <br>
+        Confirm Password:      <input class="form-control" type="password"  name="cpassword" required placeholder ="Confirm Password"/> <br>
+        First Name:            <input class="form-control" type="text" name="first_name" required placeholder ="First Name" /> <br>
+        Last Name:           <input class="form-control" type="text" name="last_name" required placeholder ="Second Name"/> <br>
         Admin: <br>
 
     <input type="radio" name="admin" value=1/>Yes<br>
@@ -44,7 +27,20 @@ foreach($_POST as $key=>$value) {
    
         <input  type="submit" name="submit" value="Register" /> 
         
+        <?php  $msg = "";
+            if(isset($_POST['submit'])){
+            
+    
+            if ($_POST['password'] !== $_POST['cpassword']){
+            $msg = "Password does not match";
         
+        
+    }   else {
+        $this->model->register_new_admin($_POST['user_id'], $_POST['email'], $_POST['first_name'], $_POST['last_name'], $_POST['password'], $_POST['admin']);
+    }
+}
+
+?>
         <span><?php echo $msg?></span>
       
         

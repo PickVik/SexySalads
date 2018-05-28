375a65eb5d0c3aef7f6571d611d417bd6609f244
ViktM <viktoria.mezei0708@gmail.com>
2018-05-28 23:17:53 +0100

    login hash

--- models/Login_model.php
+++ models/Login_model.php
@@ -10,7 +10,7 @@ class Login_model extends Model {
     // if correct then heads to admin page
     // else main page
     // PASSWORD NEEDS TO BE HASHED!!!
-<<<<<<< HEAD
+
 
 
     public function login() {
@@ -76,36 +76,4 @@ class Login_model extends Model {
     }
 
 }
-=======
     
-    
-   public function login(){
-       
-      
-        $stmt = $this->db->prepare("SELECT * FROM user WHERE email = ?");
-        $stmt->execute([$_POST['email']]);
-        $user = $stmt->fetch();
-       // $user = $user[4];
-        if ($user && password_verify($_POST['password'], $user[4]))
-{
-
-      // if ($row >0 && password_verify($_POST['password'], $data_array['password'])){
-           
-           //login
-           Session::init();
-          
-           $_SESSION['Email'] = $_POST['email'];
-           
-           header('location: ../admin');
-          
-         // NEEDS TO BE CHECKED - SHOULD TAKE YOU TO MAIN PAGE WHERE WE HAVE THE NAVBAR  
-       }else {
-           //error
-           header ('location: ../index');
-           
-       }
-       print_r($data);
-   }
-}
- 
->>>>>>> d8f2632229ede3448662578510352b4449acd513

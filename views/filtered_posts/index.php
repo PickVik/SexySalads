39c5c0889c040604d8de21c7df670c8d877e70d7
pallavidughrekar <38434226+pallavidughrekar@users.noreply.github.com>
2018-05-28 20:36:02 +0100

    pics hyperlinked for filtered_posts too

--- views/filtered_posts/index.php
+++ views/filtered_posts/index.php
@@ -30,8 +30,9 @@ require 'views/includes/navbar.php'; ?>
         
 
 		<div class="post" style="margin-left: 0px;width:49.5%;display:inline-block;padding:5px">
+                    <a href="single_post?post-slug=<?php echo $post['slug']; ?>">
 			<img src="<?php echo 'views/pictures/' . $post['image']; ?>" class="post_image" alt="$post['image']" style='width:100%'>
-			<a href="single_post?post-slug=<?php echo $post['slug']; ?>">
+			
 				<div class="post_info">
 					<h3><?php echo $post['title']; ?></h3>
 					<div class="info">
